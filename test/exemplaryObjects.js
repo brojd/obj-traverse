3c7f98597ea284f75aee47f5fa2b80465c65b2d2
dominik791 <dominik.broj@gmail.com>
2017-01-06 18:50:56 +0100

    create exemplary objects for tests

---
+++ test/exemplaryObjects.js
@@ -0,0 +1,56 @@
+const emptyObj = {};
+
+const objWithoutChildren = {
+  name: 'foo',
+  city: 'bar'
+};
+
+const objWithEmptyChildren = {
+  name: 'foo',
+  children: []
+};
+
+const objTree = {
+  name: 'foo',
+  city: 'bar',
+  children: [
+    {
+      name2: 'foo2',
+      city2: 'city2',
+      children: [
+        {
+          name5: 'name5',
+          city5: 'city5'
+        },
+        {
+          name6: 'name6',
+          city6: 'city6',
+          children: [
+            {
+              name8: 'name8'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name3: 'name3',
+      city3: 'city3',
+    },
+    {
+      name4: 'name4',
+      city4: 'city4',
+      children: [
+        {
+          name7: 'name7',
+          city7: 'city7'
+        }
+      ]
+    }
+  ]
+};
+
+
+
+
+export { emptyObj, objWithoutChildren, objWithEmptyChildren, objTree };
