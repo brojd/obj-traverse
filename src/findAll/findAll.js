622bb9565575095c62fee6c4225e51eb653211eb
dominik791 <dominik.broj@gmail.com>
2017-01-08 13:42:12 +0100

    skeleton of findAll()

---
+++ src/findAll/findAll.js
@@ -0,0 +1,5 @@
+const findAll = function () {
+  return true;
+};
+
+export { findAll };
---
+++ src/findAll/test/findAll.test.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+// import { findAll } from '../findAll.js';
+
+chai.expect();
+
+// const expect = chai.expect;
+
+describe('testing findFirst()', () => {
+
+});
