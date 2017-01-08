d0136c9ee39fd935c7d09398ec7acd2f02f0c8bd
dominik791 <dominik.broj@gmail.com>
2017-01-08 20:30:18 +0100

    initial version of findAll()

--- src/findAll/findAll.js
+++ src/findAll/findAll.js
@@ -1,5 +1,19 @@
-const findAll = function () {
-  return true;
+const findAll = function (tree, childrenKey, objToFindBy) {
+  let objToReturn = [];
+  function innerFunc(tree, childrenKey, objToFindBy) {
+    const findKey = Object.keys(objToFindBy)[0];
+    const findValue = objToFindBy[findKey];
+    if (tree[findKey] === findValue) {
+      objToReturn.push(tree);
+    }
+    if (tree.hasOwnProperty(childrenKey)) {
+      for (let n of tree[childrenKey]) {
+        innerFunc(n, childrenKey, objToFindBy);
+      }
+    }
+  }
+  innerFunc(tree, childrenKey, objToFindBy);
+  return objToReturn;
 };
 
 export { findAll };
