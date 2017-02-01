441a8bb0ad1982c60c83f86a08d78c788a9cd87c
dominik791 <dominik.broj@gmail.com>
2017-02-01 22:36:42 +0100

    export functions properly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { findFirst } from './findFirst/findFirst';
-import { findAll } from './findAll/findAll';
+import findFirst from './findFirst/findFirst';
+import findAll from './findAll/findAll';
+import findAndModifyFirst from './findAndModifyFirst/findAndModifyFirst';
+import findAndModifyAll from './findAndModifyAll/findAndModifyAll';
+import findAndDeleteFirst from './findAndDeleteFirst/findAndDeleteFirst';
+import findAndDeleteAll from './findAndDeleteAll/findAndDeleteAll';
 
-export { findFirst, findAll };
+export { findFirst, findAll, findAndModifyFirst, findAndModifyAll, findAndDeleteFirst, findAndDeleteAll };
